eb6b497f7873e0e4b849a6adb685a572a9d8e234
SiC <99667304+stevew1007@users.noreply.github.com>
2023-05-08 23:26:51 +0800

    fix(version): fix missiong packageJson import

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react';
+import packageJson from './package.json';
 
 // https://vitejs.dev/config/
 export default defineConfig({
